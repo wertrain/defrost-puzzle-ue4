005053a74e1d795453e5786895bcab90ba982c57
wertrain <wertrain@gmail.com>
2022-03-16 20:43:31 +0900

    Add game code

---
+++ DefrostPuzzle/Source/DefrostPuzzle/Game/Piece.cpp
@@ -0,0 +1,20 @@
+#include "Piece.h"
+
+Piece::Piece()
+    : m_PointX(0)
+    , m_PointY(0)
+{
+
+}
+
+Piece::Piece(const int x, const int y)
+    : m_PointX(x)
+    , m_PointY(y)
+{
+
+}
+
+Piece::~Piece()
+{
+
+}
---
+++ DefrostPuzzle/Source/DefrostPuzzle/Game/Piece.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cinttypes>
+
+class Piece
+{
+public:
+    Piece();
+    Piece(int x, int y);
+    ~Piece();
+
+    int32_t GetX() const { return m_PointX; }
+    int32_t GetY() const { return m_PointY; }
+
+private:
+    int32_t m_PointX;
+    int32_t m_PointY;
+};
